8e4a32de3061955b7be9a0309ff5817a24f15f16
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-09-22 22:08:07 -0500

    [sm] started to build out dynamic routing from taor tutorial

--- taor-api/app.py
+++ taor-api/app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/') # decorator to route the request to the function
+@app.route("/") # decorator to route the request to the function
+@app.route("/index")
+@app.route("/home") # Pro tip: you can chain decorators
 def home():
     return "Hello, World!"
+
+# Routes can contain methods
+@app.route("/api/v1/users/", methods=["GET", "POST", "PUT", "DELETE"])
+def users():
+    return "Users"
+
+# Dynamic values in routes
+@app.route("/user/<username>")
+def profile(username):
+    return f"User: {username}"
+
+@app.route("<int:year>/<int:month>/<title>") # possible types string, int, float, path
+def article(year, month, title):
+    return f"{year} - {month} - {title}"
+
+
