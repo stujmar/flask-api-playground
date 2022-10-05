628139a6c65af99a71c3d1b1ecd0e33472989ef0
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-10-04 22:15:29 -0500

    {sm] started to learn about marshalled fields, cool

--- twt-api/main.py
+++ twt-api/main.py
@@ -1,6 +1,6 @@
 from os.path import exists
 from flask import Flask, render_template
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 # Wrap our app in an API?
@@ -23,6 +23,16 @@ if not exists("database.db"):
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
+
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes on the video")
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
 
@@ -37,6 +47,13 @@ def abort_if_exists(video_id):
     if video_id in videos:
         abort(409, message="Video already exists with that ID...")
 
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
+
 # Define a resource
 class Video(Resource):
 
@@ -49,26 +66,34 @@ class Video(Resource):
         return {video_id: args}, 201
 
     # READ
+    @marshal_with(resource_fields)
     def get(self, video_id):
         result = VideoModel.query.filter_by(id=video_id).first()
         if result:
             return result, 200
         else:
             return None, 404
+
         # Old non db code
         # abort_if_video_id_doesnt_exist(video_id)
         # return videos[video_id], 200
 
     # UPDATE
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        args = video_put_args.parse_args()
-        abort_if_exists(video_id)
-        if video_id not in videos.keys():
-            videos[video_id] = args
-            return {video_id: args}, 201
-        # else:
+        arg = video_update_args.parse_args()
+        video = VideoModel(id=video_id, name=arg["name"], views=arg["views"], likes=arg["likes"])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+        # Old non db code
+        # args = video_put_args.parse_args()
+        # abort_if_exists(video_id)
+        # if video_id not in videos.keys():
         #     videos[video_id] = args
-        #     return {video_id: args}, 200
+        #     return {video_id: args}, 201
+
 
     # DELETE
     def delete(self, video_id):
